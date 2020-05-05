1a15cf119ebd53763a24d2b0f6e708c057fbf923
Jesus Raul Alarcon <jalarcon@aforecoppel.com>
2020-05-05 00:54:44 -0600

    Se agrega componente de registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { HomeComponent } from './views/home/home.component';
 import { AuthGuard } from './helpers/auth.guard';
+import { RegisterComponent } from './views/register/register.component';
 
 
 const routes: Routes = [
 
   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   
   { path: '**', redirectTo: '' }
 ];
